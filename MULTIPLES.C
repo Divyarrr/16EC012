13b4f49fe3f533d307b9ab4d3b576d2123e93c44
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-01-21 13:18:52 +0530

    Create MULTIPLES.C

---
+++ MULTIPLES.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+	#include<conio.h>
+	void main()
+	{
+	    int a,i,b;
+	    scanf("%d",&a);
+	    for(i=1;i<=5;i++)
+	    {
+	        b=a*i;
+	    printf("%d\n",b);
+	    }
+	    getch();
+	}
