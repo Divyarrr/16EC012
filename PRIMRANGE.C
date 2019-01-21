2b8144de84f74987bd5fae6868c743a8c707e881
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-01-21 13:15:41 +0530

    Create PRIMRANGE.C

---
+++ PRIMRANGE.C
@@ -0,0 +1,51 @@
+#include<stdio.h>
+	#include<conio.h>
+	void main()
+	{
+	    int a,b,f,i,j;
+	    scanf("%d%d",&a,&b);
+	    for(i>a;i<b;i++)
+	    {
+	        if(i>a&&i<b)
+	        {
+	        for(j=2;j<=i/2;j++)
+	        {
+	            if(i%j==0)
+	            {
+	                f=1;
+	                break;
+	            }
+	        }
+	        if(f==0)
+	        {
+	            printf("%d\t",i);
+	        }
+	        f=0;
+	        }
+	    }
+	    getch();
+	}
+#include<stdio.h>
+	#include<conio.h>
+	void main()
+	{
+	    int a,i,flag=0;
+	    scanf("%d",&a);
+	    for(i=2;i<=a/2;i++)
+	    {
+	        if(a%i==0)
+	        {
+	        flag=1;
+	        break;
+	        }
+	    }
+	    if(flag==0)
+	    {
+	    printf("yes");
+	    }
+	    else
+	    {
+	        printf("no");
+	    }
+	    getch();
+	}
