b431023cb97a7a4928a90eadfedb1d64d954a723
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-01-28 15:40:48 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char[100];
+    int i,s;
+    gets(a);
+    for(i=0;a[i]!=NULL;i++)
+    {
+        if(a[i]=='')
+        s++;
+    }
+    printf("%d",s+1);
+}
