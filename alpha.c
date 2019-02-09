166ee2005a958d852525c9f7aa67832b59cfe2fe
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-02-09 14:57:47 +0530

    Update alpha.c

--- alpha.c
+++ alpha.c
@@ -6,11 +6,11 @@
 	scanf("%c",&a);
 	if((a>=65&&a<=90)||(a>=97&&a<=122))
 	{  
-	printf("alphabet");
+	printf ("alphabet");
 	}
 	else
 	{
-	printf("no");
+	printf ("no");
 	}
-	getch();
+	  getch();
 	}
